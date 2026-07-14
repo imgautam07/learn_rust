a1b0adffa7b4aa6b54805b3588d46a1c4b7d2f32
Gautam Yadav <gautamyadav08112005@gmail.com>
2026-07-15 03:12:21 +0530

    day2: guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,3 @@
-use std::io;
-use std::cmp::Ordering;
-use colored::*;
-
 fn main() {
 
-    let secret_number = rand::random_range::<u16,_>(1..100);
-    println!(":: Guessing Game ({}) ::", secret_number);
-
-    loop {
-        println!("Enter a number (1-100) :");
-        let mut guess = String::new(); // mut if the value need to changed;
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        println!("Your input is: {}", guess);
-
-        let guess:u16 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("{}", "Invalid input!".red());
-                continue;
-            },
-        };
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{}", "Too small!".red()),
-            Ordering::Greater => println!("{}", "Too big!".red()),
-            Ordering::Equal => {
-                println!("{}", "You win!".green());
-                break;
-            }
-        }
-        println!("The secret number is: {}", secret_number);
-    }
 }
